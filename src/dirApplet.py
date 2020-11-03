ebf60e94a1a3d9a5047fe2c784626a0740e2ef6a
Connor Spangler <connormspangler@gmail.com>
2020-11-02 18:08:12 -0800

    Finished direction labels

--- src/dirApplet.py
+++ src/dirApplet.py
@@ -1,27 +1,31 @@
-from PyQt5.QtCore import QPoint, Qt
+from PyQt5.QtCore import QPoint, QSize, Qt
 from PyQt5.QtGui import (
-    QBrush, QColor,
+    QBrush, QColor, QFont,
     QPainter,
     QPen
 )
 from PyQt5.QtWidgets import (
     QDialog,
-    QGridLayout,
+    QGridLayout, QLabel, QSizePolicy,
     QWidget
 )
 from math import (
     hypot,
     floor,
     atan2,
-    pi
+    pi,
+    sin,
+    cos,
+    radians
 )
 
 
 class compassWedge(QWidget):
-    def __init__(self, start_angle, w, h, parent=None):
+    def __init__(self, start_angle, w, h, o, parent=None):
         super().__init__(parent)
         self.start = start_angle
         self.setFixedSize(w, h)
+        self.offset = o
         self.setMouseTracking(True)
         self.active = False
 
@@ -38,7 +42,14 @@ class compassWedge(QWidget):
         pen.setWidth(5)
         painter.setBrush(brush)
         painter.setPen(pen)
-        painter.drawPie(50, 50, 500, 500, self.start, 45 * 16)
+        painter.drawPie(
+            self.offset,
+            self.offset,
+            self.width() - self.offset * 2,
+            self.height() - self.offset * 2,
+            self.start,
+            45 * 16
+        )
         painter.end()
 
     def activate(self, is_active):
@@ -49,10 +60,10 @@ class compassWedge(QWidget):
 class compassApplet(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.w_width = 600
-        self.w_height = 600
-        self.center = QPoint(self.w_width/2, self.w_height/2)
-        self.setFixedSize(self.w_width, self.w_height)
+        self.span = 600
+        self.offset = 75
+        self.center = QPoint(self.span/2, self.span/2)
+        self.setFixedSize(self.span, self.span)
         self.setMouseTracking(True)
 
         self.wedges = []
@@ -60,11 +71,55 @@ class compassApplet(QWidget):
             self.wedges.append(
                 compassWedge(
                     start,
-                    self.w_width,
-                    self.w_height,
+                    self.span,
+                    self.span,
+                    self.offset,
                     self
                 )
             )
+
+        deg_font = QFont("Consolas", 18, 2)
+        for deg in range(8):
+            temp = QLabel(self)
+            angle = deg * 45
+            temp.setText(str(angle) + '°')
+            temp.setFont(deg_font)
+            temp.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+            tw = temp.fontMetrics().boundingRect(temp.text()).width()
+            th = temp.fontMetrics().boundingRect(temp.text()).height()
+            temp.move(self.deg_lbl_pos((180 - angle) % 360, QSize(tw, th)))
+
+    def deg_lbl_pos(self, angle, size):
+        x_0 = self.center.x()
+        y_0 = self.center.y()
+        size_x = size.width()
+        size_y = size.height()
+        off_x = 0
+        off_y = 0
+        dist = self.span/2 - self.offset + 10
+        if angle == 0:
+            off_x = -size_x/2
+        if angle == 90:
+            off_y = -size_y/2
+        if angle == 135:
+            off_y = -size_y
+        if angle == 180:
+            off_x = -size_x/2
+            off_y = -size_y
+        if angle == 225:
+            off_y = -size_y
+            off_x = -size_x
+        if angle == 270:
+            off_y = -size_y/2
+            off_x = -size_x
+        if angle == 315:
+            off_x = -size_x
+
+        theta_rad = pi/2 - radians(angle)
+        return QPoint(
+            round(x_0 + dist*cos(theta_rad) + off_x),
+            round(y_0 + dist*sin(theta_rad) + off_y)
+        )
 
     def mouseMoveEvent(self, event):
         x_1 = event.pos().x()
