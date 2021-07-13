e1766229701f336ae72abc8b6e740f8d747d0d86
Connor Spangler <connormspangler@gmail.com>
2021-07-12 21:46:33 -0700

    Implemented simple line overlay

--- src/exact_angle.py
+++ src/exact_angle.py
@@ -1,4 +1,10 @@
+from PyQt5.QtGui import (
+    QFont,
+    QPainter,
+    QPen
+)
 from PyQt5.QtCore import (
+    Qt,
     pyqtSlot
 )
 from PyQt5.QtWidgets import (
@@ -11,14 +17,54 @@ from PyQt5.QtWidgets import (
 )
 
 
+class Overlay(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAttribute(Qt.WA_NoSystemBackground)
+        self.setAttribute(Qt.WA_TransparentForMouseEvents)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        super().paintEvent(event)
+        painter.setRenderHint(QPainter.Antialiasing)
+        pen = QPen(Qt.red)
+        pen.setWidth(5)
+        painter.setPen(pen)
+        painter.drawLine(0, 10, self.parent().width() * 3, 10)
+        painter.drawLine(0, 59, self.parent().width() * 3, 59)
+        painter.end()
+
+
+class AngleContainer(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.hundreds = AngleSet()
+        self.tens = AngleSet()
+        self.ones = AngleSet()
+        layout = QHBoxLayout()
+        layout.addWidget(self.hundreds)
+        layout.addWidget(self.tens)
+        layout.addWidget(self.ones)
+        self.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+        overlay = Overlay(self)
+        overlay.setGeometry(self.geometry())
+
+
 class AngleSet(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
         layout = QVBoxLayout()
+        layout.setSpacing(0)
         for digit in range(10):
             tmp_btn = QPushButton(str(digit))
+            tmp_btn.setFixedSize(100, 50)
+            tmp_btn.setFont(QFont('Consolas', 16))
             tmp_btn.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
             layout.addWidget(tmp_btn)
         self.setLayout(layout)
@@ -28,16 +74,11 @@ class ExactAngle(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.hundreds = AngleSet()
-        self.tens = AngleSet()
-        self.ones = AngleSet()
+        self.angle_container = AngleContainer()
         layout = QHBoxLayout()
         layout.addStretch()
-        layout.addWidget(self.hundreds)
-        layout.addWidget(self.tens)
-        layout.addWidget(self.ones)
+        layout.addWidget(self.angle_container)
         layout.addStretch()
-        self.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
         self.setLayout(layout)
 
     @pyqtSlot(int)
