b538e891c16d357655308c15bc773c7fbf6120f9
Connor Spangler <connormspangler@gmail.com>
2021-07-21 13:11:17 -0700

    Imp'd high-res DPI scaling

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
 from pectin import pectin
+import sys
 
 if __name__ == "__main__":
+    if hasattr(Qt, 'AA_EnableHighDpiScaling'):
+        QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+        QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     app = QApplication(sys.argv)
     window = pectin()
     window.resize(1920, 1080)
